8fa59ba85869be0c4b627ae5ead6d280ec6f607e
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-12-08 12:43:12 +0900

    修正

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,4 +1,4 @@
-import { Text, useScroll } from "@react-three/drei";
+import { useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { foodItems } from "./foodItems";
